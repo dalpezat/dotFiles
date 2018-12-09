753da448d1ca600ecf1e2dd8e8ab2b7ac42bd3dd
dalpezat <dalpezat@gmail.com>
2018-12-09 13:16:42 -0500

    Update brew.sh

--- osx/brew.sh
+++ osx/brew.sh
@@ -19,33 +19,16 @@ brew linkapps macvim
 brew install caskroom/cask/brew-cask
 
 #brew cask install java
-#brew cask install bettertouchtool
-#brew cask install firefox
 #brew cask install google-chrome
-#brew cask install google-drive
-#brew cask install iterm2
+brew cask install google-drive
 brew tap caskroom/versions
 brew cask install iterm2-nightly
-#brew cask install lastpass
 brew cask install radiant-player
-#brew cask install skype
-#brew cask install sourcetree
-#brew cask intall gitkraken
-#brew cask install xamarin
-#brew cask install xtrafinder
-#brew cask install virtualbox
-#brew cask install spotify
-#brew cask install owasp-zap
-#brew cask install android-file-transfer
+brew cask install keepassxc
 brew cask install postman
 
 brew cask install visual-studio-code-insiders
-#brew cask install royal-tsx
-
-#brew cask install microsoft-office
 
 #brew cask install haroopad
-#brew cask install microsoft-lync
-#brew cask install microsoft-lync-plugin
 
 brew cleanup
