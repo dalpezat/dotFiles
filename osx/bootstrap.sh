41916e1d2ec2e90b348919374c0aa900c9a0c940
dalpezat <dalpezat@gmail.com>
2018-12-30 18:25:15 -0500

    Update bootstrap.sh

--- osx/bootstrap.sh
+++ osx/bootstrap.sh
@@ -9,5 +9,8 @@ cd ~
 git clone https://github.com/gmarik/vundle.git .vim/bundle/vundle
 vim +:BundleInstall
 
+# finish install of oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
 # Run the android tool to choose install tools
 android
