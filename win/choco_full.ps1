f450f5de092b57a3f43b63ee2b527ba2a3a1879a
dalpezat <dalpezat@gmail.com>
2019-01-21 15:16:18 -0500

    Update choco_full.ps1

--- win/choco_full.ps1
+++ win/choco_full.ps1
@@ -1,12 +1,10 @@
 ﻿# General Utils
-choco install ditto -y
-#choco install autohotkey_l -y
 choco install agentransack -y
 choco install ripgrep -y
 choco install 7zip -y
 #choco install RSAT.FeaturePack -y
-#choco install bleachbit -y
-#choco install ccleaner -y
+choco install bleachbit -y
+choco install ccleaner -y
 #choco install ccenhancer -y
 choco install RdcMan -y
 choco install putty -y
@@ -18,13 +16,8 @@ choco install awscli -y
 #docker
 choco install -y docker-toolbox -ia /COMPONENTS="kitematic,virtualbox,dockercompose" --force
 
-# Other
-#choco install spotify -y
-
 # Browsers
 choco install googlechrome -y
-#choco install firefox -y
 
 #Web Dev
 choco install nodejs-lts -y
-#choco install ctags -y
