8e4dff170c131868e71689dfc68dcdf60a865275
dalpezat <dalpezat@gmail.com>
2020-06-16 10:19:26 -0400

    Create appInstall.sh

---
+++ lnx/appInstall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt update && sudo apt full-upgrade -y
+
+function install {
+  which $1 &> /dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Installing: ${1}..."
+    sudo apt install -y $1
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+# Basics
+install awscli
+install chromium-browser
+install curl
+install git
+install nmap
+install tree
+install vim
